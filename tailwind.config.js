c6068eead487e08b15166075a236e2d61a8add41
bdfpplinsinga <daniel@burovoordeboeg.nl>
2022-08-09 17:06:09 +0200

    created component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   content: ['./src/**/*.njk', './src/**/*.md',],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'cyan': 'hsl(158, 36%, 37%)',
+        'cream': 'hsl(30, 38%, 92%)',
+        'dark-blue': 'hsl(212, 21%, 14%)',
+        'gray-blue': 'hsl(228, 12%, 48%)',
+        'white': 'hsl(0, 0%, 100%)',
+      },
+      fontFamily: {
+        'sans': ['Montserrat'],
+        'serif': ['Fraunces'],
+      }
+    },
   },
   plugins: [],
 }
